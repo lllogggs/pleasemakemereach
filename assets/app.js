19f80cea46884dbbaeaab38498416c786518df02
lllogggs <159713119+lllogggs@users.noreply.github.com>
2025-09-06 14:44:46 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -25,7 +25,7 @@
 
   // ===== 설정 상수 =====
   // (1) URL 로깅 엔드포인트: 네 Apps Script 웹앱 URL
-  const LOG_ENDPOINT = 'https://script.google.com/macros/s/AKfycbznvhLbnqpbJQ0dMuaNix6BtZDtslylMi61RTUfBdM8IMKeE5vxFmlzP7HEseY5Lm61/exec';
+  const LOG_ENDPOINT = 'https://script.google.com/macros/s/AKfycbx-J8fHcPqMCy6OOdCTSP-g8IgPGpB1vPtWtIQf56hjaLWLKgtATjvNx2yoGKW55KFE/exec';
 
   const AFF_AFFIX = 'Allianceid=6624731&SID=225753893&trip_sub1=&trip_sub3=D4136351';
 
