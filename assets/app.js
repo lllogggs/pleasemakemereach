820eb4d89913f74e671026a82f61070f7e937400
lllogggs <159713119+lllogggs@users.noreply.github.com>
2025-09-08 16:11:08 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -24,8 +24,8 @@
   }
 
   // ===== 설정 상수 =====
-  const EXPAND_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzTZxzsNays4DPOparjopGmB6FByrQsCeYyvtwcmHygMLxNsQbVkH_E8Rs9ROjySoHg/exec';
-  const LOG_ENDPOINT    = 'https://script.google.com/macros/s/AKfycbzTZxzsNays4DPOparjopGmB6FByrQsCeYyvtwcmHygMLxNsQbVkH_E8Rs9ROjySoHg/exec';
+  const EXPAND_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxQD_dXmXr8L_qJOopbY09xoLfz--KsCjeecGnrJ_byo-ps3O8M_6G-C7e6mvLncoY/exec';
+  const LOG_ENDPOINT    = 'https://script.google.com/macros/s/AKfycbxQD_dXmXr8L_qJOopbY09xoLfz--KsCjeecGnrJ_byo-ps3O8M_6G-C7e6mvLncoY/exec';
 
   const AFF_AFFIX = 'Allianceid=6624731&SID=225753893&trip_sub1=&trip_sub3=D4136351';
 
